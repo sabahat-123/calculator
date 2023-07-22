ca96851f75cafb776f58436aa57235b3a57dfcb1
sabahat-123 <sabahat.parveen@imtspl.net>
2023-07-22 22:28:22 +0530

    update

---
+++ calculator/src/OperationButton.js
@@ -0,0 +1,12 @@
+import { ACTION } from "./App"
+
+export default function OperationButton({dispatch , operation}){
+    return(
+        <button onClick={()=>dispatch({
+            type:ACTION.CHOOSE_OPERATION,
+            payload:{operation}
+        })}>
+        {operation}
+        </button>
+    )
+}
